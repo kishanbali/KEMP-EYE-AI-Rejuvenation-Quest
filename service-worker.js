721964487325a6f2dccb277e6d326da1df9f41c4
KishanBali <kishansaraiya9@gmail.com>
2026-09-15 12:19:41 +0530

    Refresh PWA cache for mobile install update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='kemp-rejuvenation-pwa-v2';
-const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+const CACHE='kemp-rejuvenation-pwa-v3';
+const CORE=['./','./index.html','./manifest.json?v=3','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./'))));});
